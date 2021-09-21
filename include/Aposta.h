8c9f1a3b1f6b79734647f170019abf020a980888
João Victor <silvajoao720@gmail.com>
2021-09-21 20:13:49 -0300

    Update Aposta.h
    Encapsulamento adicionado

--- include/Aposta.h
+++ include/Aposta.h
@@ -6,13 +6,25 @@
 class Aposta
 {
     public:
+        Aposta();
+
+        void deletarAposta(); //zera todos os atributos
+
+        void setNomeApostador(std::string nome);
+        void setAposta(int aposta);
+        void setNumeroCartela(int numCart);
+        void setQuantiaApostada(float quantia);
+
+        std::string getNomeApostador();
+        int getAposta();
+        int getNumeroCartela();
+        float getQuantiaApostada();
+
+    private:
         std::string nomeApostador;
         int aposta;
         int numeroCartela;
         float quantiaApostada;
-
-        Aposta(); // não faz nada
-        void deletarAposta(); //zera todos os atributos
 };
 
 #endif // APOSTA_H
