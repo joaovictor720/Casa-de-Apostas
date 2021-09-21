6f235f52f589769fa3199b4f35309c30f80d0c62
João Victor <silvajoao720@gmail.com>
2021-09-21 20:11:32 -0300

    Update Evento.cpp
    Encapsulamento da classe Evento

--- src/Evento.cpp
+++ src/Evento.cpp
@@ -7,3 +7,29 @@ Evento::Evento()
     nApostadores = 0;
     resultadoFinal = 0;
 }
+
+void Evento::setNomeEvento(std::string nome){
+    nomeEvento = nome;
+}
+void Evento::setPremio(float premio){
+    premioAcumulado = premio;
+}
+void Evento::setNumApostadores(int nApostadores){
+    this->nApostadores = nApostadores;
+}
+void Evento::setResultado(int result){
+    resultadoFinal = result;
+}
+
+std::string Evento::getNomeEvento(){
+    return nomeEvento;
+}
+float Evento::getPremio(){
+    return premioAcumulado;
+}
+int Evento::getNumApostadores(){
+    return nApostadores;
+}
+int Evento::getResultado(){
+    return resultadoFinal;
+}
