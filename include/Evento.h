b66d7897ac46c8b16d632a8214b09d5aefbe6c49
João Victor <silvajoao720@gmail.com>
2021-09-21 20:12:59 -0300

    Update Evento.h
    Encapsulamento adicionado

--- include/Evento.h
+++ include/Evento.h
@@ -2,19 +2,30 @@
 #define EVENTO_H
 #define MAX_APOSTAS 100
 
-#include "Aposta.h"
+#include "Aposta2.h"
 
 class Evento
 {
     public:
+        Aposta apostas[MAX_APOSTAS]; // array de objetos da classe Aposta
+
+        Evento(); // construtor
+
+        void setNomeEvento(std::string nome);
+        void setPremio(float premio);
+        void setNumApostadores(int nApostadores);
+        void setResultado(int result);
+
+        std::string getNomeEvento();
+        float getPremio();
+        int getNumApostadores();
+        int getResultado();
+
+    private:
         std::string nomeEvento;
         float premioAcumulado;
         int nApostadores;
         int resultadoFinal;
-        Aposta apostas[MAX_APOSTAS]; // array de objetos da classe Aposta
-        // acho que isso é a tal da herança
-
-        Evento(); // construtor inicia algumas variáveis; olhar o .cpp
 };
 
 #endif // EVENTO_H
