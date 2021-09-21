e97bef65cfc9b5494d17e438a6ad880dcba48143
João Victor <silvajoao720@gmail.com>
2021-09-21 20:12:14 -0300

    Update Aposta.cpp
    Encapsulamento da classe Aposta

--- src/Aposta.cpp
+++ src/Aposta.cpp
@@ -11,3 +11,29 @@ void Aposta::deletarAposta(){
     numeroCartela = 0;
     quantiaApostada = 0;
 }
+
+void Aposta::setNomeApostador(std::string nome){
+    nomeApostador = nome;
+}
+void Aposta::setAposta(int aposta){
+    this->aposta = aposta;
+}
+void Aposta::setNumeroCartela(int numCart){
+    numeroCartela = numCart;
+}
+void Aposta::setQuantiaApostada(float quantia){
+    quantiaApostada = quantia;
+}
+
+std::string Aposta::getNomeApostador(){
+    return nomeApostador;
+}
+int Aposta::getAposta(){
+    return aposta;
+}
+int Aposta::getNumeroCartela(){
+    return numeroCartela;
+}
+float Aposta::getQuantiaApostada(){
+    return quantiaApostada;
+}
