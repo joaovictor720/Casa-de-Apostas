c099a2967fbe6527f8ced43ddd442e665753d6c1
João Victor <silvajoao720@gmail.com>
2021-10-14 10:32:34 -0300

    Update Evento.cpp

--- src/Evento.cpp
+++ src/Evento.cpp
@@ -2,17 +2,64 @@
 
 Evento::Evento()
 {
-    nomeEvento = "";
+    for (int i = 0; i < MAX_APOSTAS; i++)
+        apostas[i] == NULL;
+
+    nomeEvento = "<NOME>";
     premioAcumulado = 0;
     nApostadores = 0;
-    resultadoFinal = "";
+    resultadoFinal = "INDEFINIDO";
+}
+
+float Evento::getTotalPulesVenc(){
+    float totalPules = 0;
+    for(int i = 0; i < nApostadores; i++){
+        if (apostas[i]->getAposta() == resultadoFinal){
+            totalPules += apostas[i]->getQuantiaApostada();
+        }
+    }
+    return totalPules;
+}
+
+float Evento::getRateio(){
+    return premioAcumulado / getTotalPulesVenc();
+}
+
+int Evento::getIndiceAposta(std::string apEscolhida){
+    int i;
+    bool apostaExiste = true;
+
+    for (i = 0; i < nApostadores; i++){
+        if (apostas[i]->getNomeApostador() == apEscolhida){
+            apostaExiste = true;
+            break;
+        }
+        apostaExiste = false;
+    }
+    if (apostaExiste == false){
+        return -1;
+    }
+    return i;
+}
+
+void Evento::addPremio(float add){
+    premioAcumulado += add;
+}
+void Evento::subPremio(float sub){
+    premioAcumulado -= sub;
+}
+void Evento::addNumApostadores(){
+    nApostadores++;
+}
+void Evento::subNumApostadores(){
+    nApostadores--;
 }
 
 void Evento::setNomeEvento(std::string nome){
     nomeEvento = nome;
 }
 void Evento::setPremio(float premio){
-    premioAcumulado = premio;
+    premioAcumulado += premio;
 }
 void Evento::setNumApostadores(int nApostadores){
     this->nApostadores = nApostadores;
