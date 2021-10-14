578cd89d624271b4c223cde58cdd10268f0c186e
João Victor <silvajoao720@gmail.com>
2021-10-14 10:31:24 -0300

    Update Aposta.h

--- include/Aposta.h
+++ include/Aposta.h
@@ -7,24 +7,28 @@ class Aposta
 {
     public:
         Aposta();
+        virtual ~Aposta();
 
-        void deletarAposta(); //zera todos os atributos
-
+        // setters
         void setNomeApostador(std::string nome);
         void setAposta(std::string aposta);
         void setNumeroCartela(int numCart);
         void setQuantiaApostada(float quantia);
+        void setPremioRecebido(float premio);
 
+        // getters
         std::string getNomeApostador();
         std::string getAposta();
         int getNumeroCartela();
         float getQuantiaApostada();
+        float getPremioRecebido();
 
-    private:
+    protected:
         std::string nomeApostador;
         std::string aposta;
         int numeroCartela;
         float quantiaApostada;
+        float premioRecebido;
 };
 
 #endif // APOSTA_H
