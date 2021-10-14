fc9b0b64a6e50107b532383c98a4c3db8f0a2224
João Victor <silvajoao720@gmail.com>
2021-10-14 10:31:47 -0300

    Update Evento.h

--- include/Evento.h
+++ include/Evento.h
@@ -1,21 +1,35 @@
 #ifndef EVENTO_H
 #define EVENTO_H
-#define MAX_APOSTAS 100
+#define MAX_APOSTAS 50
 
 #include "Aposta.h"
 
 class Evento
 {
     public:
-        Aposta apostas[MAX_APOSTAS]; // array de objetos da classe Aposta
+        Aposta *apostas[MAX_APOSTAS]; // array de ponteiros para classe Aposta
 
-        Evento(); // construtor
+        Evento();
 
+        float getRateio();
+        float getTotalPulesVenc();
+        int getIndiceAposta(std::string apEscolhida);
+
+        // adders
+        void addPremio(float add);
+        void addNumApostadores();
+
+        // subtractors
+        void subPremio(float sub);
+        void subNumApostadores();
+
+        // std setters
         void setNomeEvento(std::string nome);
         void setPremio(float premio);
         void setNumApostadores(int nApostadores);
         void setResultado(std::string result);
 
+        // std getters
         std::string getNomeEvento();
         float getPremio();
         int getNumApostadores();
